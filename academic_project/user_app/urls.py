e65274c183934485f0dd09991fcb22d513d8e030
yassine ibhir <yibhir101@gmail.com>
2021-05-19 21:42:11 -0400

    modified search classes, templates,and added one time messages for login..

--- academic_project/user_app/urls.py
+++ academic_project/user_app/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
+
 
 from . import views
 
 urlpatterns = [
     path('user_register/', views.register, name='user-register'),
-    path('user_login/', LoginView.as_view(template_name = 'user_app/login.html'), name ='user-login'),
-    path('user_logout/', LogoutView.as_view(template_name = 'user_app/logout.html'), name ='user-logout'),
+    path('user_login/', views.UserLoginView.as_view(), name ='user-login'),
+    path('user_logout/', views.UserLogoutView.as_view(), name ='user-logout'),
 ]
--- academic_project/user_app/views.py
+++ academic_project/user_app/views.py
@@ -1,3 +1,5 @@
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import render
 from .forms import UserRegistrationForm, MemberFileForm
 from django.contrib import messages
@@ -6,6 +8,8 @@ from django.contrib.auth.models import User
 from .models import Member
 
 
+# Yassine Ibhir
+# validate and register user
 def register(request):
     if request.method == 'POST':
         register_form = UserRegistrationForm(request.POST)
@@ -24,7 +28,7 @@ def register(request):
             customer_profile.save()
             messages.success(request, 'Gongratulations {0}, You are registered'.format(username))
             return redirect('user-login')
-        else :
+        else:
             messages.error(request, "Unsuccessful registration. Invalid information.")
     else:
 
@@ -33,3 +37,23 @@ def register(request):
     template_name = 'user_app/register.html'
     context = {'register_form': register_form, 'file_form': file_form}
     return render(request, template_name, context)
+
+
+# Yassine Ibhir
+# Inherits from LoginView and SuccessMessageMixin
+# to login a user and provide a message
+class UserLoginView(SuccessMessageMixin, LoginView):
+    template_name = 'user_app/login.html'
+    success_message = 'You are logged in, Welcome.'
+
+
+# Yassine Ibhir
+# Inherits from LogoutView and overides dispatch to add a message when user is logged out
+# to logout a user and provide a message
+class UserLogoutView(LogoutView):
+    template_name = 'user_app/logout.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        response = super().dispatch(request, *args, **kwargs)
+        messages.info(request, 'You are logged out, GoodBye.')
+        return response
